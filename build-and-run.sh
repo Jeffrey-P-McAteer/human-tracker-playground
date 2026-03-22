8665b666a9fd8ff26b1fe7f1db5b2fc0dea8b063
Jeffrey McAteer <jeffrey@jmcateer.com>
2026-03-22 12:15:13 -0400

    misc

---
+++ build-and-run.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -ex
+
+arduino-cli compile --fqbn arduino:avr:uno
+
+deploy_program() {
+  arduino-cli upload -p /dev/ttyACM0 --fqbn arduino:avr:uno
+}
+
+while true; do
+    read -p "Do you wish to deploy this program? " yn
+    case $yn in
+        [Yy]* ) deploy_program ; break;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
