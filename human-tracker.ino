dee2fa2f548b2e58abfec0638f59e32d6521dc43
Jeffrey McAteer <jeffrey@jmcateer.com>
2026-03-24 18:19:00 -0400

    misc

--- human-tracker.ino
+++ human-tracker.ino
@@ -1,8 +1,11 @@
 
 #include "utils.h"
 
+#include "RadarSensor.h"
+
 #define LED_PIN_NO 13
 #define BUTTON_PIN_NO 2
+#define RADAR_TX_PIN_NO 4
 
 #define ON 1
 #define OFF 0
@@ -16,6 +19,8 @@ int led_state;
 
 int button_state;
 
+RadarSensor radar(RADAR_TX_PIN_NO,3); // RX, TX pins to sensor TX, RX pins
+
 void setup() {
   ms_since_led_toggled = 0;
   ms_to_toggle_led_at = 1000;
@@ -27,6 +32,10 @@ void setup() {
 
   pinMode(LED_PIN_NO, OUTPUT);
   pinMode(BUTTON_PIN_NO, INPUT);
+
+  radar.begin(256000);
+
+  printf("Bootup complete!\n");
 }
 
 void loop() {
@@ -42,12 +51,12 @@ void loop() {
   if (ms_since_led_toggled >= ms_to_toggle_led_at) {
     // Decide to toggle light
     if (led_state == ON) {
-      printf("Toggling pin %d LOW\n", LED_PIN_NO);
+      //printf("Toggling pin %d LOW\n", LED_PIN_NO);
       digitalWrite(LED_PIN_NO, LOW);
       led_state = OFF;
     }
     else {
-      printf("Toggling pin %d HIGH\n", LED_PIN_NO);
+      //printf("Toggling pin %d HIGH\n", LED_PIN_NO);
       digitalWrite(LED_PIN_NO, HIGH);
       led_state = ON;
     }
@@ -56,4 +65,15 @@ void loop() {
 
   delay(1);
   ms_since_led_toggled += 1;
+
+  if (radar.update()) {
+    RadarTarget tgt = radar.getTarget();
+    Serial.print("X (mm): "); Serial.println(tgt.x);
+    Serial.print("Y (mm): "); Serial.println(tgt.y);
+    Serial.print("Distance (mm): "); Serial.println(tgt.distance);
+    Serial.print("Angle (degrees): "); Serial.println(tgt.angle);
+    Serial.print("Speed (cm/s): "); Serial.println(tgt.speed);
+    Serial.println("-------------------------");
+
+  }
 }
