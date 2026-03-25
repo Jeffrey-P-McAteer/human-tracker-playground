64bb11a2a419a10914b3ea90de45cc62ae98addc
Jeffrey McAteer <jeffrey@jmcateer.com>
2026-03-25 16:25:18 -0400

    debug++

--- human-tracker.ino
+++ human-tracker.ino
@@ -13,58 +13,65 @@
 #define PRESSED HIGH
 #define NOT_PRESSED LOW
 
-int ms_since_led_toggled;
-int ms_to_toggle_led_at;
-int led_state;
+// int ms_since_led_toggled;
+// int ms_to_toggle_led_at;
+// int led_state;
 
-int button_state;
+// int button_state;
 
 RadarSensor radar(RADAR_TX_PIN_NO,3); // RX, TX pins to sensor TX, RX pins
 
 void setup() {
-  ms_since_led_toggled = 0;
-  ms_to_toggle_led_at = 1000;
+  // ms_since_led_toggled = 0;
+  // ms_to_toggle_led_at = 1000;
 
-  button_state = NOT_PRESSED;
-  led_state = OFF;
+  // button_state = NOT_PRESSED;
+  // led_state = OFF;
 
-  utils_setup();
+  // utils_setup();
 
-  pinMode(LED_PIN_NO, OUTPUT);
-  pinMode(BUTTON_PIN_NO, INPUT);
+  // pinMode(LED_PIN_NO, OUTPUT);
+  // pinMode(BUTTON_PIN_NO, INPUT);
+
+  //pinMode(RADAR_TX_PIN_NO, INPUT);
+  Serial.begin(9600);
+
+  while (!Serial) {
+    delay(1);
+  }
 
   radar.begin(256000);
 
-  printf("Bootup complete!\n");
+  Serial.println("Bootup complete!");
 }
 
 void loop() {
-  button_state = digitalRead(BUTTON_PIN_NO);
+  // button_state = digitalRead(BUTTON_PIN_NO);
 
-  if (button_state == PRESSED) {
-    ms_to_toggle_led_at = 100;
-  }
-  else {
-    ms_to_toggle_led_at = 1000;
-  }
+  // if (button_state == PRESSED) {
+  //   ms_to_toggle_led_at = 100;
+  // }
+  // else {
+  //   ms_to_toggle_led_at = 1000;
+  // }
 
-  if (ms_since_led_toggled >= ms_to_toggle_led_at) {
-    // Decide to toggle light
-    if (led_state == ON) {
-      //printf("Toggling pin %d LOW\n", LED_PIN_NO);
-      digitalWrite(LED_PIN_NO, LOW);
-      led_state = OFF;
-    }
-    else {
-      //printf("Toggling pin %d HIGH\n", LED_PIN_NO);
-      digitalWrite(LED_PIN_NO, HIGH);
-      led_state = ON;
-    }
-    ms_since_led_toggled = 0;
-  }
+  // if (ms_since_led_toggled >= ms_to_toggle_led_at) {
+  //   // Decide to toggle light
+  //   if (led_state == ON) {
+  //     //printf("Toggling pin %d LOW\n", LED_PIN_NO);
+  //     digitalWrite(LED_PIN_NO, LOW);
+  //     led_state = OFF;
+  //   }
+  //   else {
+  //     //printf("Toggling pin %d HIGH\n", LED_PIN_NO);
+  //     digitalWrite(LED_PIN_NO, HIGH);
+  //     led_state = ON;
+  //   }
+  //   ms_since_led_toggled = 0;
+  // }
 
   delay(1);
-  ms_since_led_toggled += 1;
+  // ms_since_led_toggled += 1;
 
   if (radar.update()) {
     RadarTarget tgt = radar.getTarget();
